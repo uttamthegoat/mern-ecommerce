6180811bbb301115efb2e3353e1011823730b449
uttamthegoat <19516uttam@gmail.com>
2023-08-30 21:19:48 +0530

    Changed routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,9 +7,14 @@ const {
   deleteProduct,
 } = require('../controllers/productController');
 
-router.post('/create', createProduct);
-router.get('/products', getProducts);
-router.put('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
+// router.post('/create', createProduct);
+// router.get('/products', getProducts);
+// router.put('/products/:id', updateProduct);
+// router.delete('/products/:id', deleteProduct);
+
+router.route('/create-product').post(createProduct);
+router.route('/get-products').get(createProduct);
+router.route('/edit-product').put(createProduct);
+router.route('/delete-product').delete(createProduct);
 
 module.exports = router;
