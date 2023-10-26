311f67df41ea3e7335ec057af3b5c6a7484b2eb7
uttamthegoat <19516uttam@gmail.com>
2023-10-26 21:06:31 +0530

    Navbar completed

--- client/src/components/Static/Navbar.jsx
+++ client/src/components/Static/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./styles/Navbar.module.css";
 import {
   navBrand,
