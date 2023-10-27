bf2db04b041a5abd2c528acf0359a06852c37865
uttamthegoat <19516uttam@gmail.com>
2023-10-27 21:09:48 +0530

    Completed Footer

--- client/src/components/Static/Footer.jsx
+++ client/src/components/Static/Footer.jsx
@@ -1,46 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
+import styles from "./styles/Navbar.module.css";
 import { Link } from "react-router-dom";
+import { contactMail } from "../../assets/constants";
 
-function Footer() {
-  const [contactOpen, setContactOpen] = useState(false);
-
-  const toggleContact = () => {
-    setContactOpen(!contactOpen);
-  };
+const Footer = () => {
   return (
-    <footer className="bg-gray-200 py-4 mt-auto">
-      <div className="text-center">
-        <div className="text-gray-600">
-          <button
-            className="bg-transparent border-none"
-            onClick={toggleContact}
-          >
-            Contact Us
-          </button>
-        </div>
-        {contactOpen && (
-          <div className="text-gray-600 flex flex-col">
-            <a href="mailto:19516uttam@gmail.com">19516uttam@gmail.com</a>
-            <a href="mailto:19516uttam@gmail.com">
-              Email: sumanths947@gmail.com
-            </a>
-            <a href="mailto:19516uttam@gmail.com">
-              Email: uttamthegoat@gmail.com
-            </a>
-            <a href="mailto:19516uttam@gmail.com">
-              Email: vipulnayak@gmail.com
-            </a>
-          </div>
-        )}
+    <footer className="bg-[#292929] mt-auto text-white">
+      <div className="flex flex-col sm:flex-row sm:justify-around py-5">
+        {/* Website Info */}
+        <section className="px-8 py-4">
+          <h1 className="mb-4 font-bold text-xl md:text-2xl">Website Info</h1>
+          <ul className="ps-5 space-y-3">
+            <li className={`${styles.Footer_Custom_li}`}>
+              <Link to="/" className="font-semibold  ms-2">
+                Home
+              </Link>
+            </li>
+            <li className={`${styles.Footer_Custom_li}`}>
+              <Link to="/about" className="font-semibold  ms-2">
+                About Us
+              </Link>
+            </li>
+            <li className={`${styles.Footer_Custom_li}`}>
+              <Link
+                to="/terms"
+                className="font-semibold ms-2 hover:cursor-pointer"
+              >
+                Terms of Use
+              </Link>
+            </li>
+          </ul>
+        </section>
+        {/* Contact us */}
+        <section className="px-8 py-4">
+          <h1 className="mb-4 font-bold text-xl md:text-2xl">Contact Us</h1>
+          <ul className="ps-5 space-y-3">
+            <li
+              className={`${styles.Footer_Custom_li} ${styles.Footer_Call_li}`}
+            >
+              <a href="tel:+91 8618555625" className="font-semibold  ms-2">
+                tel:+91 8618555625
+              </a>
+            </li>
+            {contactMail.map((mail) => {
+              return (
+                <li
+                  className={`${styles.Footer_Custom_li} ${styles.Footer_Mail_li}`}
+                  key={mail.id}
+                >
+                  <a href={mail.link} className="font-semibold ms-2">
+                    {mail.mail}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
       </div>
-      <div className="text-center text-gray-600">
-        <Link href="/about-us">About Us</Link>
-      </div>
-      <div className="text-center text-gray-600">
-        <Link href="/terms-of-use">Terms of Use</Link>
+      <div className="py-3">
+        <hr className="w-11/12 mx-auto mb-3" />
+        <p className="text-center text-xl px-2">
+          Copyright © All rights reserved. Theme Creation Legend by{" "}
+          <span className="text-rose-600 font-semibold">uttamthegoat</span>
+        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
