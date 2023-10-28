07b69a6f915ce4ce4b981261a073aac7ad3db449
uttamthegoat <19516uttam@gmail.com>
2023-10-28 19:48:20 +0530

    uttam branch check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,9 @@ import Navbar from "./components/Static/Navbar";
 import Sidebar from "./components/Static/Sidebar";
 import Footer from "./components/Static/Footer";
 import Alert from "./features/alert/Alert";
+import Redirect from "./pages/Redirect";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import allRoutes from "./utils/routes";
-import Redirect from "./pages/Redirect";
 
 const App = () => {
   return (
