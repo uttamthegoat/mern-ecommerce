79026d92cb818e368920e064795323453e9368a7
Vipul <vipulnayak1192@gmail.com>
2023-10-28 20:48:19 +0530

    generate and verify otp changes added to routes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -4,11 +4,15 @@ const {
   loginUser,
   logoutUser,
   registerUser,
+  generateOTP,
+  verifyOTP,
 } = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.route("/signup").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(authMiddleware, logoutUser);
+router.route("/generateOTP").post(generateOTP);
+router.route("/verifyOTP").post(verifyOTP);
 
 module.exports = router;
